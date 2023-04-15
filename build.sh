aeeedea8ba1b307f2bec21585e7b584a65600156
Edward <ing_jorge_caceres@hotmail.com>
2023-04-15 18:16:43 -0400

    pip install upgrade pip

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -o errexit
 #no se necesita poetry
 #poetry install
 #pip install -r requirements.txt
+pip install --upgrade pip
 
 python manage.py collectstatic --no-input
 python manage.py migrate
